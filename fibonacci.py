53472d47967bd2992d8352d687234727e918a1e9
11249a131-sys <11249a131@kanchiuniv.ac.in>
2025-11-10 16:01:06 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    a, b = 0, 1
+    print("Fibonacci Sequence:")
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+try:
+    N = int(input("Enter the number of terms (N > 0): "))
+    if N <= 0:
+        print("Error: Invalid input! Please enter a number greater than 0.")
+    else:
+        fibonacci(N)
+except ValueError:
+    print("Error: Please enter a valid integer.")
