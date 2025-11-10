5940ad6852d04beb2a639ce88f24c9b059f3f17b
11249a131-sys <11249a131@kanchiuniv.ac.in>
2025-11-10 15:47:19 +0530

    Update testavg.py

--- testavg.py
+++ testavg.py
@@ -1,7 +1,7 @@
-Aim:
+//Aim:
     To write a Python program that accepts three test marks from the user and calculates the average of the best two test marks.
 
- Algorithm:
+//Agorithm:
 
 Start the program.
 Input three test marks from the user — say test1, test2, and test3.
@@ -10,7 +10,7 @@ Add the other two (best) marks and divide by 2 to get the average.
 Display the best two test average.
 End the program.
 
-  Program:
+  //Program:
   
 test1 = float(input("Enter marks of Test 1: "))
 test2 = float(input("Enter marks of Test 2: "))
@@ -21,4 +21,5 @@ best_two_total = total - lowest
 best_two_average = best_two_total / 2
 print("The average of the best two test marks is:", best_two_average)
 
-Result:
+//Result:
+
