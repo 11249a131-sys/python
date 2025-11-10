1c380fa8a5c040070c0ccaf65f746926fcb3a667
11249a131-sys <11249a131@kanchiuniv.ac.in>
2025-11-10 15:56:18 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,18 @@
+//Aim:
+      To develop a Python program that checks whether a given number is a palindrome and counts the occurrences of each digit.
+
+//Algorithm:
+step 1: Start the program.
+step 2: Input a number as a string.
+step 3: Reverse the number using slicing ([::-1]).
+step 4: Compare the reversed string with the original:
+step 5: If equal → Palindrome
+step 6: Else → Not a palindrome
+step 7: For each unique digit, count how many times it appears using .count().
+stsep 8: Display the results.
+step 9: End the program.
+
+//Program:
 num = input("Enter a number: ")
 if num == num[::-1]:
     print(f"{num} is a Palindrome number.")
@@ -6,3 +21,5 @@ else:
 print("\nDigit Occurrences:")
 for digit in sorted(set(num)):
     print(f"Digit {digit} occurs {num.count(digit)} time(s).")
+
+//Result:
