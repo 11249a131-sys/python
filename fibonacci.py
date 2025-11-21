b87fb265be394ba7116e1f6671a077dc0b5882ae
11249a131-sys <11249a131@kanchiuniv.ac.in>
2025-11-21 17:00:36 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,27 @@
-def fibonacci(n):
+//Aim:To write a Python program that generates the Fibonacci sequence up to N terms, where N > 0. The program should display an error message if the entered value of N is not greater than zero.
+//Algorithm:
+1.Start the program.
+2.Accept a value N from the user.
+3.Check if N > 0:
+4.If not, display an error message and stop the program.
+5.Define a function fibonacci(N) to generate the Fibonacci sequence.
+6.Inside the function:
+7.Initialize the first two numbers as 0 and 1.
+8.Use a loop to generate the next terms using the formula:
+9.Fn = Fn-1 + Fn-2.
+10.Print the sequence up to N terms.
+11.Call the function with the user-provided value.
+12.Stop the program.
+//Program:
+def fibonacci(N):
     a, b = 0, 1
-    print("Fibonacci Sequence:")
-    for i in range(n):
+    print("Fibonacci sequence:")
+    for i in range(N):
         print(a, end=" ")
         a, b = b, a + b
-try:
-    N = int(input("Enter the number of terms (N > 0): "))
-    if N <= 0:
-        print("Error: Invalid input! Please enter a number greater than 0.")
-    else:
-        fibonacci(N)
-except ValueError:
-    print("Error: Please enter a valid integer.")
-
+N = int(input("Enter the number of terms (N > 0): "))
+if N <= 0:
+    print("Error: Please enter a value greater than 0.")
+else:
+    fibonacci(N)
+//Result: A program that generates the Fibonacci sequence up to N terms, where N > 0 is executed succesfully
